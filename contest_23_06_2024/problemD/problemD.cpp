604247ccf263c5ac04fa9a35fe2576f9e3635ed6
Félix Martins <felix.marcial@hotmail.com>
2024-07-04 16:22:04 +0100

    Added corrected problem D for contest 1986

--- contest_23_06_2024/problemD/problemD.cpp
+++ contest_23_06_2024/problemD/problemD.cpp
@@ -10,102 +10,34 @@ int main() {
 
         string s;
         cin >> s;
-        // cout << "'" << s << "'" << endl;
-        if (n >= 4 || (n == 3 && s[1] != '0')) {
-            bool result_found = false;
-            for (int i = 0; i < n; i++) {
-                if (s[i] == '0') {
-                    cout << 0 << '\n';
-                    result_found = true;
-                    break;
-                }
-            }
-            if (result_found) continue;
-        }
-
-        int smallest_nr = INT_MAX;
-        int smallest_nr_idx = -1;
-
-        int second_smallest_nr = INT_MAX;
-        int second_smallest_idx = -1;
-        for (int i = 0; i < n - 1; i++) {
-            int n1 = s[i] - '0', n2 = s[i + 1] - '0';
-            int candidate = n1 * 10 + n2;
-            if (candidate < smallest_nr) {
-                second_smallest_nr = smallest_nr;
-                second_smallest_idx = smallest_nr_idx;
-
-                smallest_nr = candidate;
-                smallest_nr_idx = i;                
-            }
-            else if (candidate < second_smallest_nr && candidate != smallest_nr) {
-                second_smallest_idx = i;
-                second_smallest_nr = candidate;
-            }
-        }
-        // if (smallest_nr % 10 == 1 && second_smallest_nr - smallest_nr == 1) {
-        //     smallest_nr = second_smallest_nr;
-        //     smallest_nr_idx = second_smallest_idx;
-        // }
-
-        // cout << "Smallest: " << smallest_nr << endl;
-        auto get_total = [&](int nr_idx, int nr) {
-            int total = 0;
-            for (int i = 0; i < n; ) {
-                if (i == nr_idx) {
-                    if (nr == 1) { // will only happen if n = 3 or n = 2, because 01
-                        if (total == 0) {
-                            total = 1;
-                        }
-                    }
-                    else {
-                        total += nr;
-                    }
-                    i += 2;
+        int ans = INT_MAX;
+        for (int i = 1; i < n; i++) {
+            vector<int> nums;
+            for (int j = 0; j < n; j++) {
+                if (j == i - 1) continue;
+                else if (j == i) {
+                    nums.push_back((s[j - 1] - '0') * 10 + (s[j] - '0'));
                 }
                 else {
-                    if (s[i] == '1') {
-
-                    }
-                    else {
-                        total += s[i] - '0';
-                    }
-                    i++;
+                    nums.push_back(s[j] - '0');
                 }
             }
-            return total;
-        };
-        int total = get_total(smallest_nr_idx, smallest_nr);
-        // cout << smallest_nr << " " << smallest_nr_idx << endl;
-        if (second_smallest_idx != -1) {
-            total = min(total, get_total(second_smallest_idx, second_smallest_nr));
+            sort(nums.begin(), nums.end());
+
+            if (nums.front() == 0) {
+                ans = 0;
+                break;
+            }
+            int curr = 0;
+            for (int num: nums) {
+                if (num != 1) {
+                    curr += num;
+                }
+            }
+            if (curr == 0) curr = 1;
+            ans = min(ans, curr);
         }
-        cout << total << endl;
-
-        // int total = 0;
-        // for (int i = 0; i < n; ) {
-        //     if (i == smallest_nr_idx) {
-        //         if (smallest_nr == 1) { // will only happen if n = 3 or n = 2, because 01
-        //             if (total == 0) {
-        //                 total = 1;
-        //             }
-        //         }
-        //         else {
-        //             total += smallest_nr;
-        //         }
-        //         i += 2;
-        //     }
-        //     else {
-        //         if (s[i] == '1') {
-
-        //         }
-        //         else {
-        //             total += s[i] - '0';
-        //         }
-        //         i++;
-        //     }
-        // }
-        // cout << total << endl;
+        cout << ans << endl;
     }
     return 0;
 }
---
+++ contest_23_06_2024/problemD/wrong_answer.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        // cout << "'" << s << "'" << endl;
+        if (n >= 4 || (n == 3 && s[1] != '0')) {
+            bool result_found = false;
+            for (int i = 0; i < n; i++) {
+                if (s[i] == '0') {
+                    cout << 0 << '\n';
+                    result_found = true;
+                    break;
+                }
+            }
+            if (result_found) continue;
+        }
+
+        int smallest_nr = INT_MAX;
+        int smallest_nr_idx = -1;
+
+        int second_smallest_nr = INT_MAX;
+        int second_smallest_idx = -1;
+        for (int i = 0; i < n - 1; i++) {
+            int n1 = s[i] - '0', n2 = s[i + 1] - '0';
+            int candidate = n1 * 10 + n2;
+            if (candidate < smallest_nr) {
+                second_smallest_nr = smallest_nr;
+                second_smallest_idx = smallest_nr_idx;
+
+                smallest_nr = candidate;
+                smallest_nr_idx = i;                
+            }
+            else if (candidate < second_smallest_nr && candidate != smallest_nr) {
+                second_smallest_idx = i;
+                second_smallest_nr = candidate;
+            }
+        }
+        // if (smallest_nr % 10 == 1 && second_smallest_nr - smallest_nr == 1) {
+        //     smallest_nr = second_smallest_nr;
+        //     smallest_nr_idx = second_smallest_idx;
+        // }
+
+        // cout << "Smallest: " << smallest_nr << endl;
+        auto get_total = [&](int nr_idx, int nr) {
+            int total = 0;
+            for (int i = 0; i < n; ) {
+                if (i == nr_idx) {
+                    if (nr == 1) { // will only happen if n = 3 or n = 2, because 01
+                        if (total == 0) {
+                            total = 1;
+                        }
+                    }
+                    else {
+                        total += nr;
+                    }
+                    i += 2;
+                }
+                else {
+                    if (s[i] == '1') {
+
+                    }
+                    else {
+                        total += s[i] - '0';
+                    }
+                    i++;
+                }
+            }
+            return total;
+        };
+        int total = get_total(smallest_nr_idx, smallest_nr);
+        // cout << smallest_nr << " " << smallest_nr_idx << endl;
+        if (second_smallest_idx != -1) {
+            total = min(total, get_total(second_smallest_idx, second_smallest_nr));
+        }
+        cout << total << endl;
+    }
+    return 0;
+}
