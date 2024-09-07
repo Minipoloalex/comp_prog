d4ef68e77a3533a4e776ae91f387e453d8bd154f
Félix Martins <felix.marcial@hotmail.com>
2024-09-07 22:05:10 +0100

    Solved problems A & B from CF div 2 contest/2003

---
+++ codeforces/2003_div2/problemB/problemB.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->ios::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a.begin(), a.end());
+        int m = n / 2;
+        cout << a[m] << '\n';
+    }
+    return 0;
+}
