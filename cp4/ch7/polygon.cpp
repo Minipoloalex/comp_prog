f2fc7445cd647ec013afde09fcea50ec5aa8f9f9
Félix Martins <felix.marcial@hotmail.com>
2025-02-10 12:02:46 +0000

    Added antipodal points to CP4 polygon template

--- cp4/ch7/polygon.cpp
+++ cp4/ch7/polygon.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef pair<int,int> ii;
+#define nx(i) ((i)+1)%n
+#define pv(i) ((i)-1+n)%n
+
 const double EPS = 1e-9;
 
 double DEG_to_RAD(double d) { return d*M_PI / 180.0; }
@@ -176,6 +181,55 @@ vector<point> CH_Andrew(vector<point> &Pts) {    // overall O(n log n)
   }
   H.resize(k);
   return H;
+}
+
+int sign(ll num) {
+    if (num < 0) return -1;
+    else if (num == 0) return 0;
+    else return 1;
+}
+
+// Takes in a convex polygon where the last vertex is not repeated
+// Note that the CH returns a repeated last vertex
+// Returns all pairs of indices of antipodal pairs
+// Only valid for p.size() >= 3;
+// Does not handle points with precision problems (assumes integer coords)
+// Modify other functions and structs (vec, toVec, cross, point) to support integer coords
+vector<ii> all_anti_podal(const vector<point> &p) {
+  int n = (int) p.size();
+  assert(n >= 3);
+
+  int p1 = 0, p2 = 0; // two "pointers"
+  vector<ii> ans;
+
+  // parallel edges should't be visited twice
+  vector<bool> vis(n, false);
+
+  for (;p1<n;p1++) {
+    // the edge that we are going to consider in this iteration
+    vec base = toVec(p[p1], p[nx(p1)]);
+
+    // the last condition makes sure that the cross products don't have the same sign
+    while (p2 == p1 || p2 == nx(p1) || 
+      sign(cross(base, toVec(p[p2], p[nx(p2)]))) == sign(cross(base, toVec(p[pv(p2)], p[p2])))) {
+        // problematic if n == 2
+        p2 = nx(p2);
+    }
+
+    if (vis[p1]) continue;
+    vis[p1] = true;
+
+    ans.push_back({p1, p2});
+    ans.push_back({nx(p1), p2});
+
+    // if both edges from p1 and p2 are parallel to each other
+    if (cross(base, toVec(p[p2], p[nx(p2)])) == 0) {
+        ans.push_back({p1, nx(p2)});
+        ans.push_back({nx(p1), nx(p2)});
+        vis[p2] = true;
+    }
+  }
+  return ans;
 }
 
 int main() {
